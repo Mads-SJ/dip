400f7d51ab1dd888ef4fa28c62dbc499fd24f145
mads-sj <mads.soenderstrup@gmail.com>
2023-09-18 09:43:43 +0200

    yatzy is playable

--- 07-yatzy/gui.js
+++ 07-yatzy/gui.js
@@ -1,4 +1,5 @@
 import * as yatzy from "./yatzy.js";
+import { getEyeElements } from "./dice-gui.js";
 
 const dices = document.querySelectorAll(".dice");
 const rollButton = document.getElementById("roll-button");
@@ -11,6 +12,11 @@ const totalInput = document.getElementById("total-value");
 function initialize() {
 	dices.forEach((dice, i) => {
 		dice.addEventListener("animationend", () => dice.classList.remove("rolling"));
+		dice.addEventListener("click", () => {
+			if (yatzy.getThrowCount() !== 0) {
+				dice.classList.toggle("hold");
+			}
+		});
 		dice.innerHTML = getEyeElements(i + 1);
 	});
 
@@ -23,22 +29,29 @@ function initialize() {
 }
 
 function roll() {
-	yatzy.throwDice([false, false, false, false, false]); // TODO: read holds from dice
+	const holds = getHolds();
+	yatzy.throwDice(holds);
 	updateAfterRoll();
 }
 
 function updateAfterRoll() {
 	const values = yatzy.getValues();
 	dices.forEach((dice, i) => {
-		dice.innerHTML = getEyeElements(values[i]);
-		dice.classList.add("rolling");
+		if (!dice.classList.contains("hold")) {
+			dice.innerHTML = getEyeElements(values[i]);
+			dice.classList.add("rolling");
+		}
 	});
 
 	numberOfRollsSpan.innerText = yatzy.getThrowCount();
 
 	const results = yatzy.getResults();
-	pointInputs.forEach((input, i) => input.value = results[i]);
-	
+	pointInputs.forEach((input, i) => {
+		if (input.disabled === false) {
+			input.value = results[i];
+		}
+	});
+
 	if (yatzy.getThrowCount() === 3) {
 		rollButton.disabled = true;
 	}
@@ -50,7 +63,21 @@ function handlePointSelection(event) {
 	}
 
 	const input = event.target;
-	input.classList.add("currently-selected");
+	input.disabled = true;
+	updateTotals(input);
+
+	pointInputs.forEach(input => {
+		if (input.disabled === false) {
+			input.value = "0";
+		}
+	});
+	yatzy.resetThrowCount();
+	numberOfRollsSpan.innerText = yatzy.getThrowCount();
+	rollButton.disabled = false;
+	dices.forEach(dice => dice.classList.remove("hold"));
+}
+
+function updateTotals(input) {
 	if (Array.prototype.indexOf.call(pointInputs, input) < 6) { // Ones to Sixes
 		const sum = parseInt(sumInput.value) + parseInt(input.value);
 		sumInput.value = sum;
@@ -61,77 +88,14 @@ function handlePointSelection(event) {
 		}
 	}
 	totalInput.value = parseInt(totalInput.value) + parseInt(input.value);
-	
-	
-	// input.disabled = true; do this at a later point
 }
 
-function getEyeElements(numberOfEyes) {
-	let face;
-	let dot = `<span class="dot"></span>`;
-
-	switch (numberOfEyes) {
-		case 1:
-			face = `<div class="face first-face">
-                ${dot}
-              </div>`;
-			break;
-		case 2:
-			face = `<div class="face second-face">
-                ${dot}
-                ${dot}
-              </div>`;
-			break;
-		case 3:
-			face = `<div class="face third-face">
-                ${dot}
-                ${dot}
-                ${dot}
-              </div>`;
-			break;
-		case 4:
-			face = `<div class="face fourth-face">
-                <div class="column">
-                  ${dot}
-                  ${dot}
-                </div>
-                <div class="column">
-                  ${dot}
-                  ${dot}
-                </div>
-              </div>`;
-			break;
-		case 5:
-			face = `<div class="face fifth-face">
-                  <div class="column">
-                  ${dot}
-                  ${dot}
-                </div>
-                <div class="column">
-                  ${dot}
-                </div>
-                <div class="column">
-                  ${dot}
-                  ${dot}
-                </div>
-              </div>`;
-			break;
-		case 6:
-			face = `<div class="face sixth-face">
-                <div class="column">
-                  ${dot}
-                  ${dot}
-                  ${dot}
-                </div>
-                <div class="column">
-                  ${dot}
-                  ${dot}
-                  ${dot}
-                </div>
-              </div>`;
-			break;
-	}
-	return face;
+function getHolds() {
+	const holds = [];
+	dices.forEach(dice => {
+		holds.push(dice.classList.contains("hold"));
+	});
+	return holds;
 }
 
 initialize();
