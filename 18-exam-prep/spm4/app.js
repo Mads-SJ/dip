3ec35bc25ec567798891b7456dc9c9b82cb7a3c9
Mads <mads.soenderstrup@gmail.com>
2023-12-13 12:25:21 +0100

    spm 4 session

--- 18-exam-prep/spm4/app.js
+++ 18-exam-prep/spm4/app.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 
-app.set('view engine', 'pug');
-app.set('views', 'templates');
+app.set("view engine", "pug");
+app.set("views", "templates");
 
-app.use(express.static('filer'));
+app.use(express.static("filer"));
 app.use(express.json());
-app.use(session({secret: 'hemmelig', saveUninitialized: true, resave: true}));
+app.use(session({ secret: "hemmelig", saveUninitialized: true, resave: true }));
 
-app.get('/', function (request, response) {
-
+app.get("/", function (request, response) {
+    response.render("index", { personer: request.session.personer });
 });
 
-app.post('/', function (request, response) {
+app.post("/", function (request, response) {
+    const person = request.body;
 
+    let personer = request.session.personer || [];
+    personer.push(person);
+    request.session.personer = personer;
+    response.status(201).render("index", {
+        personer,
+    });
 });
 
 app.listen(8080);
 
-console.log('Lytter på port 8080 ...');
+console.log("Lytter på port 8080 ...");
--- 18-exam-prep/spm4/filer/index.js
+++ 18-exam-prep/spm4/filer/index.js
@@ -1,6 +1,7 @@
 async function get(url) {
     const respons = await fetch(url);
-    if (respons.status !== 200) // OK
+    if (respons.status !== 200)
+        // OK
         throw new Error(respons.status);
     return await respons.json();
 }
@@ -9,10 +10,22 @@ async function post(url, objekt) {
     const respons = await fetch(url, {
         method: "POST",
         body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
+        headers: { "Content-Type": "application/json" },
     });
-    if (respons.status !== 201) // Created
+    if (respons.status !== 201)
+        // Created
         throw new Error(respons.status);
     return await respons.text();
 }
 
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+    const name = document.getElementById("navn").value;
+    const address = document.getElementById("adresse").value;
+    try {
+        const response = await post("http://localhost:8080", { name, address });
+        location.reload();
+    } catch (error) {
+        console.error(error);
+    }
+});
