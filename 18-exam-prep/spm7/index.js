b572fb184caa8e10d2b5823863f448ed54fe2a6c
Mads <mads.soenderstrup@gmail.com>
2024-01-04 09:53:00 +0100

    exam spm 6 and 7 done

--- 18-exam-prep/spm7/index.js
+++ 18-exam-prep/spm7/index.js
@@ -1,6 +1,7 @@
 async function get(url) {
     const respons = await fetch(url);
-    if (respons.status !== 200) // OK
+    if (respons.status !== 200)
+        // OK
         throw new Error(respons.status);
     return await respons.json();
 }
@@ -9,10 +10,43 @@ async function post(url, objekt) {
     const respons = await fetch(url, {
         method: "POST",
         body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
+        headers: { "Content-Type": "application/json" },
     });
-    if (respons.status !== 201) // Created
+    if (respons.status !== 201)
+        // Created
         throw new Error(respons.status);
     return await respons.text();
 }
 
+const div = document.querySelector("#result");
+const button = document.querySelector("button");
+async function main() {
+    loadRandomQuotes();
+}
+
+function showQuote(value, tags) {
+    div.innerHTML += `<p>${value}</p>`;
+    tags.forEach(tag => {
+        div.innerHTML += `<span>${tag}</span>`;
+    });
+}
+
+button.addEventListener("click", loadRandomQuotes);
+
+async function loadRandomQuotes() {
+    try {
+        const response = await Promise.all([
+            get("https://www.tronalddump.io/random/quote"),
+            get("https://www.tronalddump.io/random/quote"),
+            get("https://www.tronalddump.io/random/quote"),
+        ]);
+        div.innerHTML = "";
+        response.forEach(({ value, tags }) => {
+            showQuote(value, tags);
+        });
+    } catch (error) {
+        div.innerHTML = "Der er sket en fejl.";
+    }
+}
+
+main();
