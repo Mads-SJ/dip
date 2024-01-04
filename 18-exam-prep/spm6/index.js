2bcb2f0fb560168f46ec46ad0d169ce590457c4e
Mads <mads.soenderstrup@gmail.com>
2024-01-04 09:39:16 +0100

    fixed wrong folders

--- 18-exam-prep/spm6/index.js
+++ 18-exam-prep/spm6/index.js
@@ -1,6 +1,7 @@
 async function get(url) {
     const respons = await fetch(url);
-    if (respons.status !== 200) // OK
+    if (respons.status !== 200)
+        // OK
         throw new Error(respons.status);
     return await respons.json();
 }
@@ -9,10 +10,59 @@ async function post(url, objekt) {
     const respons = await fetch(url, {
         method: "POST",
         body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
+        headers: { "Content-Type": "application/json" },
     });
-    if (respons.status !== 201) // Created
+    if (respons.status !== 201)
+        // Created
         throw new Error(respons.status);
     return await respons.text();
 }
 
+const div = document.querySelector("#result");
+const button = document.querySelector("button");
+async function main() {
+    const { value, tags } = await getRandomQuote();
+    showQuote(value, tags);
+}
+
+async function getRandomQuote() {
+    const response = await get("https://www.tronalddump.io/random/quote");
+    return response;
+}
+
+async function getQuotesByTag(tag) {
+    const response = await get(
+        `https://www.tronalddump.io/search/quote?tag=${tag}`
+    );
+    return response;
+}
+
+function showQuote(value, tags) {
+    div.innerHTML += `<p>${value}</p>`;
+    tags.forEach(tag => {
+        div.innerHTML += `<span>${tag}</span>`;
+    });
+
+    document.querySelectorAll("span").forEach(span => {
+        span.addEventListener("click", handleTagClick);
+    });
+}
+
+button.addEventListener("click", handleButtonClick);
+
+async function handleButtonClick() {
+    const { value, tags } = await getRandomQuote();
+    div.innerHTML = "";
+    showQuote(value, tags);
+}
+
+async function handleTagClick() {
+    const response = await getQuotesByTag(this.innerText);
+    div.innerHTML = "";
+    console.log(response);
+    response._embedded.quotes.forEach(quote =>
+        showQuote(quote.value, quote.tags)
+    );
+}
+
+main();
